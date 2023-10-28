5e173a7f53568c610d968130ea8b9c24c7156b76
danifoj315 <danifoj315@student.polsl.pl>
2023-10-28 23:44:13 +0200

    added Message model

---
+++ src/main/java/basement/friends/backend/model/Message.java
@@ -0,0 +1,14 @@
+package basement.friends.backend.model;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@Document
+public class Message {
+    @Id
+    private String id;
+}
