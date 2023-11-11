547116f5759c20f9de28aa2fa4159ec58915ff84
Dannelysbeth <danielaf@onet.pl>
2023-11-11 23:02:57 +0100

    extended message model

--- src/main/java/basement/friends/backend/model/Message.java
+++ src/main/java/basement/friends/backend/model/Message.java
@@ -5,10 +5,20 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+
 @Data
 @Builder
 @Document
 public class Message {
     @Id
     private String id;
+
+    private User sender;
+
+    private User receiver;
+
+    private String message;
+
+    private Date postTime;
 }
