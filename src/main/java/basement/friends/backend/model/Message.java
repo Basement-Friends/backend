8359d5badaf93e4bf75a70385cc31b540f67c700
Dannelysbeth <danielaf@onet.pl>
2023-11-12 21:39:24 +0100

    added chat model

--- src/main/java/basement/friends/backend/model/Message.java
+++ src/main/java/basement/friends/backend/model/Message.java
@@ -3,20 +3,16 @@ package basement.friends.backend.model;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
 @Data
 @Builder
-@Document
 public class Message {
     @Id
     private String id;
 
     private User sender;
-
-    private User receiver;
 
     private String message;
 
