81ba0bce11b5a878631a69cb3dad3a414843117e
Dannelysbeth <danielaf@onet.pl>
2023-11-28 10:57:25 +0100

    chat update vol2

--- src/main/java/basement/friends/backend/repository/ChatRepository.java
+++ src/main/java/basement/friends/backend/repository/ChatRepository.java
@@ -1,9 +1,14 @@
 package basement.friends.backend.repository;
 
 import basement.friends.backend.model.Chat;
+import basement.friends.backend.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.Set;
+
 @Repository
 public interface ChatRepository extends MongoRepository<Chat, String> {
+    Set<Chat> getChatsByUsersIn(Collection<Set<User>> users);
 }
